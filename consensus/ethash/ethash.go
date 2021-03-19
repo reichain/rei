1d906773c672b3c52afbcdcfd1356bfcfdbabb55
jacksoom <lifengliu1994@gmail.com>
2021-03-19 19:04:15 +0800

    consensus/ethash: remove unnecessary variable definition (#22512)

--- consensus/ethash/ethash.go
+++ consensus/ethash/ethash.go
@@ -538,7 +538,6 @@ func NewShared() *Ethash {
 
 // Close closes the exit channel to notify all backend threads exiting.
 func (ethash *Ethash) Close() error {
-	var err error
 	ethash.closeOnce.Do(func() {
 		// Short circuit if the exit channel is not allocated.
 		if ethash.remote == nil {
@@ -547,7 +546,7 @@ func (ethash *Ethash) Close() error {
 		close(ethash.remote.requestExit)
 		<-ethash.remote.exitCh
 	})
-	return err
+	return nil
 }
 
 // cache tries to retrieve a verification cache for the specified block number
