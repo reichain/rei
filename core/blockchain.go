9c5facf4fd118e4c0ef5e366c274b160924a41f5
aaronbuchwald <aaron.buchwald56@gmail.com>
2021-04-30 06:47:05 -0400

    core: remove unused else branch in reorg (#22783)

--- core/blockchain.go
+++ core/blockchain.go
@@ -2165,7 +2165,6 @@ func (bc *BlockChain) reorg(oldBlock, newBlock *types.Block) error {
 					l := *log
 					if removed {
 						l.Removed = true
-					} else {
 					}
 					logs = append(logs, &l)
 				}
