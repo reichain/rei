81bc072e2a32451a3c842ac60b323b7c09fb4b87
Péter Szilágyi <peterke@gmail.com>
2021-05-31 10:21:48 +0300

    eth/ethconfig: flip the default from fast to snap sync

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -57,7 +57,8 @@ var LightClientGPO = gasprice.Config{
 
 // Defaults contains default settings for use on the Ethereum main net.
 var Defaults = Config{
-	SyncMode: downloader.FullSync,
+	SyncMode: downloader.FullSync, // TODO(acoshift): investigate why snap sync failed
+	// SyncMode: downloader.SnapSync,
 	Ethash: ethash.Config{
 		CacheDir:         "ethash",
 		CachesInMem:      2,
